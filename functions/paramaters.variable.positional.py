d0a0cb29da587bbd6c5f43deae842821755b848d
codewithsyre <augustineshoks7@gmail.com>
2025-11-06 22:56:36 +0200

    variable positional parameters

---
+++ functions/paramaters.variable.positional.py
@@ -0,0 +1,15 @@
+
+# variable positional parameters
+
+def minimum(*n):
+    # print(n)  # n is a tuple
+    if n:  # check if n is not empty
+        min_value = n[0]
+        for value in n[1:]:
+            if value < min_value:
+                min_value = value
+        print(min_value)
+
+minimum(1, 3, -7, 9)  # n = (1, 3, -7, 9) Output: -7
+minimum(8, 3, 5)      # n = (8, 3, 5) Output: 3
+minimum()             # n = () Output: nothing      
