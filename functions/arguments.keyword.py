15e634a48a0d162f3682bbc38674744ee0d1991f
codewithsyre <augustineshoks7@gmail.com>
2025-11-06 10:47:39 +0200

    Keyword Arguments

---
+++ functions/arguments.keyword.py
@@ -0,0 +1,7 @@
+
+# Keyword Arguments
+
+def fullName(firstName, middleName, lastName):
+    print(firstName, middleName, lastName)
+
+fullName(lastName="Shokane", firstName="Kholofelo", middleName="Augustine")
