0c045d40ace20004dd8a69da6090f7a7d3cf81b0
codewithsyre <augustineshoks7@gmail.com>
2025-11-07 21:34:01 +0200

    new version

--- functions/return.single.value.2.py
+++ functions/return.single.value.2.py
@@ -5,8 +5,10 @@ from functools import reduce
 from operator import mul
 
 def factorial(number):
-    return reduce(mul, range(1, number + 1), 1)
+    
+    return reduce(mul,range(1, number + 1),1)
+
 
 f = factorial(5)
-print(f)  # Output: 120
+print(f)  # Output: 120 
     
