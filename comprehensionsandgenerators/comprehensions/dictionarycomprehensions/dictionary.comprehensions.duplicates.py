50413a5a5c014e2797031892cde83cf58f92318a
codewithsyre <augustineshoks7@gmail.com>
2025-12-08 01:02:27 +0200

    dictionary and set comprehension

---
+++ comprehensionsandgenerators/comprehensions/dictionarycomprehensions/dictionary.comprehensions.duplicates.py
@@ -0,0 +1,5 @@
+
+word = 'Hello'
+
+swap = {c: c.swapcase() for c in word}
+print(swap)  # prints: {'H': 'h', 'e': 'E ', 'l': 'L', 'o': 'O'}
---
+++ comprehensionsandgenerators/comprehensions/setcomprehensions/set.comprehensions.py
@@ -0,0 +1,8 @@
+
+word = 'Hello'
+
+letter1 = {c for c in word}
+letter2 = set(c for c in word)
+
+print(letter1)  # prints: {'e', 'H', 'l', 'o'}
+print(letter2)  # prints: {'e', 'H', 'l', 'o
